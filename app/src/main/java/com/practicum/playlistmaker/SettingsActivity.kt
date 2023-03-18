6994620b050c0b411cb814e4931aee5f0cd09208
x-mike <hr.misha@yandex.ru>
2023-03-18 22:46:07 +0300

    Added EditText with search, added fonts, added callbacks TextWatcher, onSaveInstanceState, onRestoreInstanceState to SearchActivity. Added styles for night and day themes. And other things.

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,15 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -13,9 +17,42 @@ class SettingsActivity : AppCompatActivity() {
         val buttonBackArrow = findViewById<Button>(R.id.back_button)
 
         buttonBackArrow.setOnClickListener{
-            val displayMainIntent = Intent(this@SettingsActivity, MainActivity::class.java)
-            startActivity(displayMainIntent)
+            finish()
         }
 
+
+        val buttonAppShare = findViewById<Button>(R.id.app_share)
+
+        buttonAppShare.setOnClickListener {
+
+            val intentShareApp = Intent(Intent.ACTION_SEND)
+
+            intentShareApp.putExtra(Intent.EXTRA_TEXT,getString(R.string.pr_ya_android_developer))
+            intentShareApp.type = "text/plain"
+
+            startActivity(intentShareApp)
+        }
+
+        val buttonSupportWrite = findViewById<Button>(R.id.support_write)
+
+        buttonSupportWrite.setOnClickListener {
+            val intentSupportWrite = Intent(Intent.ACTION_SENDTO,Uri.parse(getString(R.string.mailto)))
+
+            intentSupportWrite.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.developer_mail)))
+            intentSupportWrite.putExtra(Intent.EXTRA_SUBJECT,getString(R.string.subject_mail))
+            intentSupportWrite.putExtra(Intent.EXTRA_TEXT,getString(R.string.body_mail))
+
+            startActivity(intentSupportWrite)
+        }
+
+        val buttonAgreement = findViewById<Button>(R.id.agreement)
+
+        buttonAgreement.setOnClickListener {
+
+            val link = getString(R.string.pr_offer)
+            val intentAgreement = Intent(Intent.ACTION_VIEW, Uri.parse(link))
+
+            startActivity(intentAgreement)
+        }
     }
-}
+ }
