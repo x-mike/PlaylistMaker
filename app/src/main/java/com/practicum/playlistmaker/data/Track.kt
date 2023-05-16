bfcf78ac0ed9af6e647aea49d432d3877ac0f857
x-mike <hr.misha@yandex.ru>
2023-05-16 23:58:24 +0300

    Sprint 13. Homework. Added player activity.

--- app/src/main/java/com/practicum/playlistmaker/data/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/Track.kt
@@ -5,5 +5,14 @@ data class Track(
     var artistName: String? = null,
     var trackTimeMillis: Long? = null,
     var artworkUrl100: String? = null,
-    var trackId: Long? = null
+    var trackId: Long? = null,
+    var collectionName: String? =null,
+    var releaseDate: String? = null,
+    var primaryGenreName: String? = null,
+    var country: String? = null
 )
+{
+
+    fun getCoverArtwork() = artworkUrl100?.replaceAfterLast('/',"512x512bb.jpg")
+
+}
